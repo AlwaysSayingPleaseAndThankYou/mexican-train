3cb209cad49bbf982e0cc383a46c41b303422b67
HotpushupGuy420 <aengus.crowley@wsu.edu>
2020-06-14 11:56:14 -0700

    added multiple players to fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,15 +3,29 @@ import pytest
 from board import Board
 from players import Player
 
+
 @pytest.fixture
 def example_board():
     test_board = Board()
     return test_board
 
+
 @pytest.fixture
 def example_player(example_board):
     return Player(0, 15, example_board)
 
+
 @pytest.fixture
-def example_players(example_board):
-    return None
+def example_players_4(example_board):
+    players_4 = []
+    for i in range(4):
+        players_4.append(Player(i, 15, example_board))
+    return players_4
+
+
+@pytest.fixture
+def example_players_6(example_board):
+    players_6 = []
+    for i in range(6):
+        players_6.append(Player(i, 12, example_board))
+    return players_6
