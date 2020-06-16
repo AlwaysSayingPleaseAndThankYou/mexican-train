d63054479bd5024739e494b0d974e72b93109da0
HotpushupGuy420 <aengus.crowley@wsu.edu>
2020-06-16 14:14:35 -0700

    wrote test for play_tile

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,2 +1,21 @@
-def test_check_opens(example_board, example_players):
+import pytest
+from board import Board
+
+
+@pytest.mark.parametrize("tile, index, opens", [
+    ([8, 1], 0, False),
+    ([10, 7], 1, True)])
+def test_play_tile(example_board, example_player, tile, index, opens):
+    # Two player game
+    # Example Player is player 0
+    example_board.opens = [True, False, True]
+    example_board.ends = [8, 10, 12]
+    # Tile should be sanitized by other functions
+    # Should only test a valid set of tiles
+    example_board.play_tile(tile)
+    assert example_board.ends[index] == tile[1]
+    assert example_board.opens[0] is opens
+
+
+def test_report_playables(example_board):
     assert False
