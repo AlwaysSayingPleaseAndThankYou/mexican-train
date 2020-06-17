311934b338650bad075d7b63a54f7c3b8288e366
HotpushupGuy420 <aengus.crowley@wsu.edu>
2020-06-17 11:36:28 -0700

    report_playables now passing test

--- board.py
+++ board.py
@@ -1,18 +1,25 @@
-"""Class and related functions to the mexican train board."""
+"""Board with all the tile ends.
+
+On the player turn, player consumes and updates the board.
+"""
 import random
 
 
 class Board:
     """Class and related values to board."""
 
-    def __init__(self, round=12, turn=0):
+    def __init__(self, num_players, trick=12, turn=0):
         """Store possible boneyard values, mexican_train, and double lock."""
-        self.round = round
+        self.trick = trick
         self.turn = turn
         self.boneyard = self._make_tiles()
         self.double = False
-        self.mexican_train = round
-        self.boneyard.remove([round, round])
+        self.mexican_train = trick
+        self.boneyard.remove([trick, trick])
+        # num_players + 1 to account for mexican train
+        # Last value is always the mexican train
+        self.opens = [False] * (num_players + 1)
+        self.ends = [trick] * (num_players + 1)
 
     def _make_tiles(self):
         """Generate all possible tiles, [0,0]=>[12,12]."""
@@ -21,6 +28,21 @@ class Board:
             for i in range(13):
                 tiles.append([j, i])
         return tiles
+
+    def report_playables(self):
+        """Return end value for open trains"""
+        playables = []
+        for val in enumerate(self.opens):
+            if val[1] is True:
+                playables.append(self.ends[val[0]])
+        return playables
+
+    def play_tile(self, tile, player_num):
+        """Take a tile from player, add it to the board"""
+        # TODO: this can replace add_to_mexican_train
+        # TODO: this necessitates a play_tile func for player to produce tile
+        # - make it part of the turn function
+        pass
 
     def deal_tile(self):
         """Give a tile out to a player, remove it from the boneyard."""
@@ -31,8 +53,3 @@ class Board:
         """Add player tile to mexican train"""
         # TODO: tile should be sanitized and checked
         self.mexican_train.append(tile)
-
-    def check_opens(self, players):
-        opens = [0] * (len(players) + 1)
-        for player in players:
-        pass
--- tests/test_mexican_train.py
+++ tests/test_mexican_train.py
@@ -1,1 +1,2 @@
 import pytest
+from mexican_train import reporter
